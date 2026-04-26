3ed5393c94cc83dd861696ee3bdece1c71ccf305
Saraswati-Waghmode <168018067+Saraswati-Waghmode@users.noreply.github.com>
2026-04-26 15:29:28 +0530

    save

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,11 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println("Hello...");
+        test();
+
+    }
+    public static void test(){
+        System.out.println("Heluuuuuuuu");
     }
 }
 
